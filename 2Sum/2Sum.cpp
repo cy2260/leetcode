a9a3476a0635262d79ab58eeddfcbe9cd1f7cc54
cy2260 <yunchao60@hotmail.com>
2013-04-20 01:34:34 -0400

    rename

--- 2Sum/2Sum.cpp
+++ 2Sum/2Sum.cpp
@@ -1,11 +1,43 @@
-/*
-Given an array of integers, find two numbers such that they add up to a specific target number.
+#include<vector>
+#include<unordered_map>
+#include<algorithm>
+#include<iostream>
 
-The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2. Please note that your returned answers (both index1 and index2) are not zero-based.
-
-You may assume that each input would have exactly one solution.
-
-Input: numbers={2, 7, 11, 15}, target=9
-Output: index1=1, index2=2
-*/
+class Solution {
+public:
+    vector<int> twoSum(vector<int> &numbers, int target) {        
+        assert( numbers.size() >= 2 );
+        
+        vector<int> res( 2, 0 );
+        unordered_map<int, vector<int>> numMap;
+       	
+	//build a map, key is the number, value is idx+1
+        for( int i = 0; i < numbers.size(); ++i ) {
+            if( numMap.count( numbers[i] ) ) {
+                numMap[numbers[i]].push_back( i + 1 );
+            }
+            else {
+                numMap[numbers[i]] = vector<int>( 1, i + 1 );
+            }
+        }
+                
+        for( int i = 0; i < numbers.size(); ++i ) {
+            if( numMap.count( target - numbers[i] ) ) {
+                if( numbers[i] != target - numbers[i] ) {
+                    res[0] = i + 1;
+                    res[1] = numMap[ target - numbers[i] ][0];
+                    return res;
+                }
+                else {
+                    if( numMap[numbers[i]].size() > 1 ) {
+                        res[0] = numMap[numbers[i]][0];
+                        res[1] = numMap[numbers[i]][1];
+                        return res;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+};
 
