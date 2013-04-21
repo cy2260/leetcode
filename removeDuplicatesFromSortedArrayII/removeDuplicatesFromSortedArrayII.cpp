e50ef436fb2e08df900fd146ffc94652dc0101b1
cy2260 <yunchao60@hotmail.com>
2013-04-20 23:54:36 -0400

    add another solution

--- removeDuplicatesFromSortedArrayII/removeDuplicatesFromSortedArrayII.cpp
+++ removeDuplicatesFromSortedArrayII/removeDuplicatesFromSortedArrayII.cpp
@@ -13,3 +13,28 @@ public:
         return curIdx+1;
     }
 };
+
+class Solution {
+public:
+    int removeDuplicates(int A[], int n) {
+        if( !A || n <= 2 ) return n;
+        
+        int curIdx = 0;
+        int count = 0;
+        
+        for( int idx = 1; idx < n; ++idx ) {
+            if( A[curIdx] == A[idx] ) {
+                ++count;
+                
+                if( count > 1 ) continue;
+            }
+            else {
+                count = 0;
+            }
+            
+            A[++curIdx] = A[idx];
+        }
+        return curIdx+1;
+    }
+};
+
