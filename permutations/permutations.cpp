3920bfdeef8ebd3d510d712ae46951974a47e404
cy2260 <yunchao60@hotmail.com>
2013-04-21 23:21:32 -0400

    add another solution

--- permutations/permutations.cpp
+++ permutations/permutations.cpp
@@ -21,3 +21,36 @@ public:
         }
     }
 }
+
+
+class Solution {
+public:
+    vector<vector<int> > permute(vector<int> &num) {
+        vector<vector<int>> res;
+        if( num.size() < 1 ) return res;
+        
+        vector<int> solution( num.size() );
+        vector<bool> visited( num.size(), false );
+        
+        perm( res, num, 0, solution, visited );
+        
+        return res;
+    }
+    
+    void perm( vector<vector<int>>& res, vector<int>& num, int idx, vector<int>& solution, vector<bool>& visited ) {
+        if( idx == num.size() ) {
+            res.push_back( solution );
+            return;
+        }
+        
+        for( int i = 0; i < num.size(); ++i ) {
+            if( visited[i] == false ) {
+                visited[i] = true;
+                solution[idx] = num[i];
+                perm( res, num, idx+1, solution, visited );
+                visited[i] = false;
+            }
+        }
+    }
+};
+
