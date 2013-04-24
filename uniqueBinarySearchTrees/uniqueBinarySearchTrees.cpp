d95689961cc8e7b128f10ceaf75949e45a42f46e
cy2260 <yunchao60@hotmail.com>
2013-04-23 23:28:38 -0400

    virgin

---
+++ uniqueBinarySearchTrees/uniqueBinarySearchTrees.cpp
@@ -0,0 +1,40 @@
+
+class Solution {
+public:
+    unordered_map<int, int> resMap;
+    int numTrees(int n) {
+        if( n == 0 || n == 1 ) return 1;
+        
+        if( resMap.find( n ) != resMap.end() ) {
+            return resMap[n];
+        }
+        
+        int count = 0;
+        for( int i = 0; i < n; ++i ) {
+            count += numTrees( i ) * numTrees( n-i-1 );
+        }
+        resMap[n] = count;
+        
+        return count;  
+    }
+};
+
+
+class Solution {
+public:
+
+    int numTrees(int n) {
+        if( n == 0 || n == 1 ) return 1;
+        int dp[n];
+        memset( dp, 0, sizeof(int)*n );
+        dp[0] = dp[1] = 1;
+        
+        for( int i = 2; i < n; ++i ) {
+            for( int j = 0; j < i; ++j ) {
+                dp[i] += dp[j]*dp[i-j-1];
+            }
+        }
+        return dp[n-1];     
+    }
+};
+
