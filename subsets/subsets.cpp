0fe78c168ee496eaec559cd6ef2a6eb72138c68f
cy2260 <yunchao60@hotmail.com>
2013-04-25 00:40:37 -0400

    generic backtracking

--- subsets/subsets.cpp
+++ subsets/subsets.cpp
@@ -17,3 +17,36 @@ public:
         return res;
     }
 };
+
+class Solution {
+public:
+    vector<vector<int> > subsets(vector<int> &S) {
+        vector<vector<int>> res;
+        if( S.size() < 1 ) return res;
+        
+        sort(S.begin(), S.end());
+        
+        vector<bool> sol( S.size(), false );
+        
+        genSubsets( res, S, 0, sol );
+
+        return res;
+    }
+    
+    void genSubsets( vector<vector<int>>& res, vector<int> &S, int idx, vector<bool>& sol ) {
+        if( idx == S.size() ) {
+            vector<int> subset;
+            for( int i = 0; i < sol.size(); ++i ) {
+                if( sol[i] == true ) subset.push_back( S[i] );
+            }
+            res.push_back( subset );
+            return;
+        }
+        
+        genSubsets( res, S, idx+1, sol );
+        sol[idx] = true;
+        genSubsets( res, S, idx+1, sol );
+        sol[idx] = false;
+    }
+};
+
