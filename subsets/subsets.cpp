e13567760686f3f11fd879f62ebed7f2ef495f14
cy2260 <yunchao60@hotmail.com>
2013-04-25 20:23:10 -0400

    add classic sol

--- subsets/subsets.cpp
+++ subsets/subsets.cpp
@@ -24,6 +24,32 @@ public:
         vector<vector<int>> res;
         if( S.size() < 1 ) return res;
         
+        sort( S.begin(), S.end() );
+        vector<int> sol;
+        
+        dfs( S, res, 0, sol );
+        
+        return res;
+    }
+    
+    void dfs( vector<int> &S, vector<vector<int>>& res, int idx, vector<int>& sol ) {
+        res.push_back( sol );
+        
+        for( int i = idx; i < S.size(); ++i ) {
+            sol.push_back( S[i] );
+            dfs( S, res, i+1, sol );
+            sol.pop_back();        
+        }
+    }
+};
+
+
+class Solution {
+public:
+    vector<vector<int> > subsets(vector<int> &S) {
+        vector<vector<int>> res;
+        if( S.size() < 1 ) return res;
+        
         sort(S.begin(), S.end());
         
         vector<bool> sol( S.size(), false );
