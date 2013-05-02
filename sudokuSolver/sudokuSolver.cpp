a7d5620c3d09f6fd8bb1957b9eefbef11bab34f0
cy2260 <yunchao60@hotmail.com>
2013-05-02 00:08:39 -0400

    refactor sudokuSolver

--- sudokuSolver/sudokuSolver.cpp
+++ sudokuSolver/sudokuSolver.cpp
@@ -31,3 +31,52 @@ public:
         return true;
     }
 };
+
+class Solution {
+public:
+    void solveSudoku(vector<vector<char> > &board) {
+        solveHelper( board, 0, 0 );
+    }
+    
+    bool solveHelper( vector<vector<char> > &board, int row, int col ) {
+        if( !findNextPosition( board, row, col ) ) return true; //no empty position, solved
+        
+        vector<char> nums;
+        getValidNums( board, nums, row, col );
+        for( int i = 0; i < nums.size(); ++i ) {
+            board[row][col] = nums[i];
+            if( solveHelper( board, row, col ) ) return true;
+            board[row][col] = '.';
+        }
+        return false;
+    }
+    
+    bool findNextPosition( vector<vector<char> > &board, int& row, int& col ) {
+        for( int i = 0; i < 9; ++i ) {
+            for( int j = 0; j < 9; ++j ) {
+                if( board[i][j] == '.' ) {
+                    row = i;
+                    col = j;
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    
+    void getValidNums( vector<vector<char> > &board, vector<char>& nums, int row, int col ) {
+        bool used[10] = {false};
+        for( int i = 0; i < 9; ++i ) {
+            if( board[row][i] != '.' )  used[board[row][i]-'0'] = true;
+            if(  board[i][col] != '.' ) used[board[i][col]-'0'] = true;
+            int m = row/3*3 + i/3;
+            int n = col/3*3 + i%3;
+            if( board[m][n] != '.' )    used[board[m][n]-'0'] = true;
+        }
+        
+        for( int i = 1; i <= 9; ++i ) {
+            if( used[i] == false ) nums.push_back( i+'0' );
+        }
+    }
+};
+
