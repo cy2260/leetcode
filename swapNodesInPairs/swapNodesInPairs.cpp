5c1e022ee736fabbb52dc848a9d4d66fe7eaae66
cy2260 <yunchao60@hotmail.com>
2013-05-02 23:33:29 -0400

    iterative solution

--- swapNodesInPairs/swapNodesInPairs.cpp
+++ swapNodesInPairs/swapNodesInPairs.cpp
@@ -19,3 +19,35 @@ public:
         return head;
     }
 };
+
+class Solution {
+public:
+    ListNode *swapNodes( ListNode* a, ListNode* b ) {
+        a->next = b->next;
+        b->next = a;
+        return b;
+        
+    }
+    
+    ListNode *swapPairs(ListNode *head) {
+        if( !head )       return NULL;
+        if( !head->next ) return head;
+
+        ListNode* currNode = head;
+        ListNode* prevNode = NULL;
+        
+        while( currNode ) {
+            ListNode* nextNode = currNode->next;
+            if( !nextNode ) break;
+            ListNode* tempNode = swapNodes( currNode, nextNode );
+            if( prevNode == NULL )  head = tempNode;
+            else                    prevNode->next = tempNode;
+            prevNode = currNode;
+            currNode = currNode->next;
+            
+        }
+        
+        return head;
+    }
+};
+
