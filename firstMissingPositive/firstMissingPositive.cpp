c493f072b4820420c0285c6a02696a63b16b2dfa
cy2260 <yunchao60@hotmail.com>
2013-05-05 23:08:40 -0400

    virgin

---
+++ firstMissingPositive/firstMissingPositive.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int firstMissingPositive(int A[], int n) {
+        if( !A || n < 1 ) return 1;
+        
+        int idx = 0;
+        while( idx < n ) {
+            if( A[idx] > 0 && A[idx] < n && A[idx] != idx+1 && A[idx] != A[A[idx]-1] ) {
+                swap( A[idx], A[A[idx]-1] );
+            }
+            else  ++idx;
+        }
+        
+        for( idx = 0; idx < n; ++idx ) {
+            if( A[idx] != idx+1 ) break;
+        }
+        
+        return idx+1;
+    }
+};
