898683cb38dd9eae98a035a160e2583cf7279f83
cy2260 <yunchao60@hotmail.com>
2013-05-09 00:30:19 -0400

    refactor

--- sortColors/sortColors.cpp
+++ sortColors/sortColors.cpp
@@ -2,6 +2,32 @@ class Solution {
 public:
     void sortColors(int A[], int n) {
         if( !A || n < 1 ) return;
+        int idx       = 0;
+        int redEnd    = 0;
+        int blueStart = n-1;
+        
+        while( idx <= blueStart ) {
+            if( A[idx] == 0 ) {
+                if( idx > redEnd )   swap( A[idx], A[redEnd] );
+                else                 ++idx;
+                
+                ++redEnd;
+            }
+            else if( A[idx] == 2 ) {
+                if( idx < blueStart )  swap( A[idx], A[blueStart] );
+                else                   ++idx;
+                
+                --blueStart;
+            }
+            else  ++idx;
+        }
+    }
+};
+
+class Solution {
+public:
+    void sortColors(int A[], int n) {
+        if( !A || n < 1 ) return;
         
         int* count = new int[3];
         memset( count, 0x00, sizeof(int)*3);
