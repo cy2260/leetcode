78a4bb780127cf7b8ba9f6a47bdc72fd9fb973bf
cy2260 <yunchao60@hotmail.com>
2013-06-15 22:11:59 -0400

    virgin

--- maxSlidingWindow/maxSlidingWindow.cpp
+++ maxSlidingWindow/maxSlidingWindow.cpp
@@ -1,6 +1,28 @@
+typedef pair<int, int> Pair;
+
 vector<int> maxSlidingWindow( vector<int> nums, int w ) {
     vector<int> res;
     if( nums.size() < w ) return res;
 
     priority_queue<Pair> q;
+
+    for( int i = 0; i < w; ++i ) {
+        q.push( Pair( nums[i], i ) );
+    }
+
+    for( int i = w; i < nums.size(); ++i ) P
+        Pair tmpPair = q.top();
+	res.push( tmpPair.first );
+
+	while( tmpPair->second <= i-w ) {
+	   q.pop();
+	   tmpPair = q.top();
+	}
+
+	q.push( Pair( nums[i], i ) );
+    }
+    res.push( tmpPair.first );
+    return res;
+}
+
 
